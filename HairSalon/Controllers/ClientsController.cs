188858a8f1b5492bdfe45641954a95dee0ca9cce
Woo Jin Kim <kimwoojin211@gmail.com>
2021-03-12 11:09:15 -0800

    create Client Index() View and Controller functionality.

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -1,7 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using HairSalon.Models;
 namespace HairSalon.Controllers
 {
   public class ClientsController : Controller
   {
+    private readonly HairSalonContext _db;
+
+    public ClientsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Client> model = _db.Clients.Include(model => model.Stylist).ToList();
+      return View(model);
+    }
   }
 }
